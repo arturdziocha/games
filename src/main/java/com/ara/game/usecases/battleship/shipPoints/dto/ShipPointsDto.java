53e48f51098290b5de28750ddca1db88d4653c95
arturdziocha <artur.dziocha@gmail.com>
2021-01-12 21:05:59 +0100

    ss

---
+++ src/main/java/com/ara/game/usecases/battleship/shipPoints/dto/ShipPointsDto.java
@@ -0,0 +1,42 @@
+package com.ara.game.usecases.battleship.shipPoints.dto;
+
+import com.ara.game.usecases.battleship.point.dto.PointDto;
+import com.ara.game.usecases.battleship.ship.dto.ShipDto;
+import io.vavr.collection.Set;
+
+public class ShipPointsDto {
+    private final ShipDto ship;
+    private final Set<PointDto> points;
+
+    public static class Builder {
+        private ShipDto ship;
+        private Set<PointDto> points;
+
+        public Builder ship(final ShipDto ship) {
+            this.ship = ship;
+            return this;
+        }
+
+        public Builder points(final Set<PointDto> points) {
+            this.points = points;
+            return this;
+        }
+
+        public ShipPointsDto build() {
+            return new ShipPointsDto(this);
+        }
+    }
+
+    private ShipPointsDto(Builder builder) {
+        this.ship = builder.ship;
+        this.points = builder.points;
+    }
+
+    public ShipDto getShip() {
+        return ship;
+    }
+
+    public Set<PointDto> getPoints() {
+        return points;
+    }
+}
