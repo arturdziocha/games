666d51fd413a409456fc71ded2a4c5e23da1cc69
arturdziocha <artur.dziocha@gmail.com>
2021-02-19 21:04:45 +0100

    Updated player ships

---
+++ src/main/java/com/ara/game/usecases/battleship/playerShips/PlayerShipFacade.java
@@ -0,0 +1,39 @@
+package com.ara.game.usecases.battleship.playerShips;
+
+import com.ara.game.usecases.battleship.playerShips.dto.PlayerShipCreateDto;
+import com.ara.game.usecases.battleship.playerShips.dto.PlayerShipDto;
+import com.ara.game.usecases.battleship.playerShips.port.PlayerShipGateway;
+import com.ara.game.usecases.battleship.ship.port.ShipGateway;
+import com.ara.game.usecases.battleship.shipPoints.dto.ShipPointsDto;
+import com.ara.game.usecases.battleship.shipPoints.port.ShipPointsGateway;
+import com.ara.game.usecases.common.Error;
+import com.google.inject.Inject;
+import io.vavr.collection.Set;
+import io.vavr.control.Either;
+
+public class PlayerShipFacade {
+    private final Creator creator;
+    private final Finder finder;
+    private final Remover remover;
+
+    @Inject
+    public PlayerShipFacade(final PlayerShipGateway playerShipGateway, final ShipGateway shipGateway,
+                            final ShipPointsGateway shipPointsGateway) {
+        this.creator = new Creator(playerShipGateway, shipGateway, shipPointsGateway);
+        this.finder = new Finder(playerShipGateway);
+        this.remover = new Remover(playerShipGateway);
+    }
+
+    public Either<Error, PlayerShipDto> create(final PlayerShipCreateDto inputData) {
+        return creator.create(inputData);
+    }
+
+    public Either<Error, Set<ShipPointsDto>> find(final String playerId) {
+        return finder.findAll(playerId);
+    }
+
+    public void remove(final String playerId) {
+        remover.remove(playerId);
+    }
+
+}
