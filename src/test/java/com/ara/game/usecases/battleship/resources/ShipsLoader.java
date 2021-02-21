8849c973a4e2e3cfcdd3609afae3af6b1c4e1d68
arturdziocha <artur.dziocha@gmail.com>
2021-02-21 21:47:51 +0100

    Updated player ships

--- src/test/java/com/ara/game/usecases/battleship/resources/ShipsLoader.java
+++ src/test/java/com/ara/game/usecases/battleship/resources/ShipsLoader.java
@@ -1,5 +1,25 @@
 package com.ara.game.usecases.battleship.resources;
 
-final class ShipsLoader {
+import com.ara.game.usecases.battleship.ship.ShipFacade;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import external.ConsoleModule;
+import io.vavr.collection.Set;
 
+
+final class ShipsLoader {
+    private final ShipFacade shipFacade;
+    private final PointsLoader pointsLoader;
+
+    ShipsLoader() {
+        Injector injector = Guice.createInjector(new ConsoleModule());
+        shipFacade = injector.getInstance(ShipFacade.class);
+        pointsLoader = new PointsLoader();
+    }
+
+    public Set<String> loadAllShips() {
+        Set<String> points = pointsLoader.loadSixStartPoints();
+        //TODO
+        return null;
+    }
 }
