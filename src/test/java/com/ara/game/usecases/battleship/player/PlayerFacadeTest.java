53616221bcc76c584a16d23c4057b789ecca4050
arturdziocha <artur.dziocha@gmail.com>
2021-03-24 13:54:37 +0100

    added validator tests for Player.class added Mockito dependencies

--- src/test/java/com/ara/game/usecases/battleship/player/PlayerFacadeTest.java
+++ src/test/java/com/ara/game/usecases/battleship/player/PlayerFacadeTest.java
@@ -26,60 +26,12 @@ class PlayerFacadeTest {
     }
 
     @Test
-    @DisplayName("Should return Either.left when input data is null")
-    void test1() {
-        // Given
-        PlayerCreateDto input = null;
-        // When
-        Either<Error, CreateDto> player = playerFacade.create(input);
-        // Then
-        assertThat(player.getLeft().getCause()).isEqualTo("Data cannot be empty");
-    }
-
-    @Test
-    @DisplayName("Should return Either.left when player name is null")
-    void test2() {
-        // Given
-        PlayerType playerType = PlayerType.HUMAN_PLAYER;
-        PlayerCreateDto input = new PlayerCreateDto.Builder().name(null).playerType(playerType).build();
-        // When
-        Either<Error, CreateDto> player = playerFacade.create(input);
-        // Then
-        assertThat(player.getLeft().getCause()).isEqualTo("Name cannot be empty");
-    }
-
-    @Test
-    @DisplayName("Should return Either.left when player type is null")
-    void test3() {
-        // Given
-        PlayerCreateDto input = new PlayerCreateDto.Builder().name("Artur").playerType(null).build();
-        // When
-        Either<Error, CreateDto> player = playerFacade.create(input);
-        // Then
-        assertThat(player.getLeft().getCause()).isEqualTo("Type of player cannot be empty");
-    }
-
-    @Test
-    @DisplayName("Should return Either.left when player type is not found")
-    void test45() {
-        // Given
-        PlayerCreateDto input = new PlayerCreateDto.Builder().name("Artur").playerType(null).build();
-        // When
-        Either<Error, CreateDto> player = playerFacade.create(input);
-        // Then
-        assertThat(player.getLeft().getCause()).isEqualTo("Type of player cannot be empty");
-    }
-
-    @Test
     @DisplayName("Should create player")
     void test5() {
         // Given
         PlayerType playerType = PlayerType.HUMAN_PLAYER;
         // When
-        PlayerCreateDto input = new PlayerCreateDto.Builder()
-                .name("Artur")
-                .playerType(playerType)
-                .build();
+        PlayerCreateDto input = new PlayerCreateDto.Builder().name("Artur").playerType(playerType).build();
         Either<Error, CreateDto> player = playerFacade.create(input);
 
         // Then
