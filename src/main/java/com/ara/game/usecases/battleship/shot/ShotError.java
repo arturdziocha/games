f7662f4d2a33b0d3e617b576b64075d294dc81ad
arturdziocha <artur.dziocha@gmail.com>
2021-04-08 11:39:58 +0200

    Removed unnecessary errors

--- src/main/java/com/ara/game/usecases/battleship/shot/ShotError.java
+++ src/main/java/com/ara/game/usecases/battleship/shot/ShotError.java
@@ -5,10 +5,8 @@ import com.ara.game.usecases.common.Error;
 enum ShotError implements Error {
     DATA_CANNOT_BE_EMPTY("Data cannot be empty"),
     GAME_NOT_STARTED("Game not started"),
-    POINT_ALREADY_SHOOTED("Point is already shooted"),
-    PLAYER_DATA_CANNOT_BE_EMPTY("Player data cannot be empty"),
-    POINT_DATA_CANNOT_BE_EMPTY("Point data cannot be empty"),
-    OPPONENT_DATA_CANNOT_BE_EMPTY("Opponent data cannot be empty"),
+    POINT_ALREADY_SHOOTED("Point is already shooted"),    
+    POINT_DATA_CANNOT_BE_EMPTY("Point data cannot be empty"),    
     PERSISTENCE_FAILED("Persistence failed");
     private final String cause;
 
