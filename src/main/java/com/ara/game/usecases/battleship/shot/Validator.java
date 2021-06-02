77e00a0f1ea7e6ef5377ba810f39c8815fcd9173
Artur <artur.dziocha@gmail.com>
2021-06-02 11:07:52 +0200

    refactored Validator

--- src/main/java/com/ara/game/usecases/battleship/shot/Validator.java
+++ src/main/java/com/ara/game/usecases/battleship/shot/Validator.java
@@ -2,19 +2,20 @@ package com.ara.game.usecases.battleship.shot;
 
 import com.ara.game.usecases.battleship.shot.dto.ShotCreateDto;
 import com.ara.game.usecases.common.Error;
+import io.vavr.control.Either;
 import io.vavr.control.Option;
 
 final class Validator {
-    Option<Error> validate(ShotCreateDto inputData) {
+    Either<Error, ShotCreateDto> validate(ShotCreateDto inputData) {
         if (inputData == null) {
-            return Option.some(ShotError.DATA_CANNOT_BE_EMPTY);
+            return Either.left(ShotError.DATA_CANNOT_BE_EMPTY);
         }
         if (!inputData.getGame().isStarted()) {
-            return Option.some(ShotError.GAME_NOT_STARTED);
+            return Either.left(ShotError.GAME_NOT_STARTED);
         }
         if (inputData.getPoint() == null) {
-            return Option.some(ShotError.POINT_DATA_CANNOT_BE_EMPTY);
+            return Either.left(ShotError.POINT_DATA_CANNOT_BE_EMPTY);
         }
-        return Option.none();
+        return Either.right(inputData);
     }
 }
